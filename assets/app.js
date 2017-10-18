6c42056811a6db382968b09085a22971f0049914
David Benton <soccershelbyb15@gmail.com>
2017-10-17 20:55:46 -0400

    firbase integration initial

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,41 @@
+/////////////////////Shelby's Code/////////////////////////
+var config = {
+    apiKey: "AIzaSyBdq4yukufhzQNtwWMLxLI8Ps-cMkPf1T0",
+    authDomain: "beercation-8cecd.firebaseapp.com",
+    databaseURL: "https://beercation-8cecd.firebaseio.com",
+    projectId: "beercation-8cecd",
+    storageBucket: "beercation-8cecd.appspot.com",
+    messagingSenderId: "151097719331"
+};
+
+firebase.initializeApp(config);
+
+// Create a variable to reference the database.
+var database = firebase.database();
+
+// Initial Values
+var emailArray = [];
+var email = "";
+
+//submits email to an array on click
+$("#submit").on('click', function(){
+  event.preventDefault();
+  
+  email = $('#email-input').val();
+
+  database.ref().push({
+        emailAddress: email,
+      });
+
+  database.ref().on("child_added", function(snapshot){
+    emailAddress = snapshot.val().emailAddress;
+    emailArray.push(emailAddress);
+    console.log(emailArray);
+    });
+
+});
+//////////////////////End Shelby's Code////////////////////////
+
 var addressVicinity = [];
         var map, places, infoWindow;
         var markers = [];
